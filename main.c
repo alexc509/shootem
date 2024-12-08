8b13fbf0dd2ad23a6ac00c6cdb8e19e2c38471be
alex <alexcool574@gmail.com>
2024-12-08 01:40:08 -0600

    Added 2 movable square players

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include "raylib.h"
 
-int main() {
-    InitWindow(800, 600, "Hello Damian");
+int main(void) {
+    InitWindow(800, 600, "Shoot");
     
-    while (!WindowShouldClose()) {
+
+    // Square properties
+    Vector2 squarePosition_1 = {100.0f, 100.0f};
+    Vector2 squarePosition_2 = {700.0f, 100.0f};
+    float squareSize = 25.0f;
+    float speed = 5.0f;
+
+    SetTargetFPS(60);
+
+    // Main game looop
+    while (!WindowShouldClose()) { 
+
+        // Update
+        if (IsKeyDown(KEY_D)) squarePosition_1.x += speed;
+        if (IsKeyDown(KEY_A)) squarePosition_1.x -= speed;
+        if (IsKeyDown(KEY_W)) squarePosition_1.y -= speed; 
+        if (IsKeyDown(KEY_S)) squarePosition_1.y += speed;       
+
+        if (IsKeyDown(KEY_RIGHT)) squarePosition_2.x += speed;
+        if (IsKeyDown(KEY_LEFT)) squarePosition_2.x -= speed;
+        if (IsKeyDown(KEY_UP)) squarePosition_2.y -= speed;
+        if (IsKeyDown(KEY_DOWN)) squarePosition_2.y += speed;   
+
+        // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Welcome to your demise damian!", 190, 200, 20, DARKGRAY);
+
+        DrawRectangle(squarePosition_1.x, squarePosition_1.y, squareSize, squareSize, BLUE);
+        DrawRectangle(squarePosition_2.x, squarePosition_2.y, squareSize, squareSize, RED);
         EndDrawing();
     }
 
